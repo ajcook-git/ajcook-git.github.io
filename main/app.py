d1899662cab5c52234c7adccf9f56f3a59a48885
Adam <mail.ajcook@gmail.com>
2021-02-02 15:14:00 +0000

    new app

--- main/app.py
+++ main/app.py
@@ -8,21 +8,20 @@ import pandas as pd
 
 app = dash.Dash(__name__)
 
-df = pd.read_csv("./data/data.csv")
+df = pd.read_csv("../data/data.csv")
 
-fig1 = px.line(df, x='act_starttime', y='act_distance', color='name')
+fig1 = px.line(
+    df,
+    x='act_starttime',
+    y='act_distance',
+    color='name',
+    labels=dict(
+        act_starttime='Start time',
+        act_distance='Distance (m)'
+    ),
+    title='Time vs. Distance (Feb Challenge)'
+)
 
-app.layout = html.Div([
-    html.H1("Strava App"),
-    dcc.Graph(id='example_strava', figure=fig1)
-])
+fig1.to_html('../index.html')
 
-#@app.callback(
-#    Output("graph", "figure"), 
-#    [Input("dropdown", "value")])
-#def display_color(color):
-#    fig = go.Figure(
-#        data=go.Bar(y=[2, 3, 1], marker_color=color))
-#    return fig
-
-app.run_server(debug=True, port=8050, host="0.0.0.0")
+print('Created ../index.html')
